584e7c7a9804bea31bd3fd1ee89ccf1dba034cd5
beefchief <c.lang.1993@gmail.com>
2024-09-23 19:11:17 -0500

    hw part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
-
+console.log(puppyList);
+const [puppies, setPuppies] = useState(puppyList);
+console.log(puppies);
   return (
     <>
-      <div>
+      <div className ="App">
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
